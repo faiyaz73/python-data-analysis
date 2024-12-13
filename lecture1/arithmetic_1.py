a7ffc13b02a25ee0bae38a00430e5caff2e2f196
faiyaz ansari <131226634+faiyaz73@users.noreply.github.com>
2024-12-13 18:46:33 +0000

    arithmatic

---
+++ lecture1/arithmetic_1.py
@@ -0,0 +1,48 @@
+# Operator	Name	Example	
+# +	Addition	x + y	
+# -	Subtraction	x - y	
+# *	Multiplication	x * y	
+# /	Division	x / y	
+# %	Modulus	x % y	
+# **	Exponentiation	x ** y	
+# //	Floor division	x // y	
+
+# question = > a grocery store a sells a bag of ice for $1.25 and makes 20% profit. if it sells 
+# 500 bags of ice, how much total profit does it make ?
+cost_ice_bag = 1.25
+profit_margin = .2
+profit_per_bag =  profit_margin * cost_ice_bag
+No_of_bag = 500
+
+total_profit = No_of_bag * (profit_per_bag)
+# print(total_profit)
+
+
+# Python Comparison Operators
+
+# Operator	Name	   Example	
+# ==	          Equal 	x == y	
+# !=	      Not equal 	x != y	
+# >	   Greater than 	x > y	
+# <	      Less than	    x < y	
+# >=	   Greater than or equal to	x >= y	
+# <=	Less than or equal to	x <= y
+
+
+a = 5
+b = 5
+# print(a==b)
+
+my_feb_num = 1
+my_least_feb_num = 5
+a_neutral_num = 3
+
+# print(my_feb_num == 1)
+# print(my_least_feb_num == a_neutral_num)
+# print(my_feb_num == a_neutral_num)
+# print(my_feb_num != a_neutral_num)
+# print(my_feb_num >= 1)
+
+
+
+
